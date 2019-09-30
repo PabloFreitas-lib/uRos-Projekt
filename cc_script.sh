a6bb46b70053c7efdb087c188d52e52d485be564
PabloFreitasUfsc <pablo.santos1999@gmail.com>
2019-09-30 13:05:40 -0300

    att 2

--- cc_script.sh
+++ cc_script.sh
@@ -2,15 +2,20 @@
 
 echo micro-ROS agent cross-compailing for Raspberry-Pi 3
 
+echo att2
 
 #sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 #counter=1
 #while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+apt-get update
+
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-apt-get update
 apt install wget git python3-vcstool
+
+apt-get update
 
 python3 -m pip install -U pyparted
 
