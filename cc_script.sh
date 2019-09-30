36e219c892d08afce20354ad9f330767e8cc5a3a
PabloFreitasUfsc <pablo.santos1999@gmail.com>
2019-09-30 12:50:03 -0300

    repository for python3-vcstool

--- cc_script.sh
+++ cc_script.sh
@@ -7,7 +7,8 @@ echo micro-ROS agent cross-compailing for Raspberry-Pi 3
 #counter=1
 #while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 apt-get update
 apt install wget git python3-vcstool
 
