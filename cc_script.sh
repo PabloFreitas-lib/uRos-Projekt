1c203d6b83833bc6859cc351bd7c665222820092
Pablo Freitas Santos <pablo_agua@hotmail.com>
2019-10-01 14:14:25 -0300

    Update cc_script.sh

--- cc_script.sh
+++ cc_script.sh
@@ -7,10 +7,10 @@ echo att2
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 #counter=1
 #while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
-apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+#apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 
 
-#apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 apt-get update
 
